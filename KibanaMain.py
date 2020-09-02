ac0a848e2b2871f16292c949c1120536d3d6dc22
U-ITSERVICES\jq998g <jq998g@CACDTL05VS0217.ITServices.sbc.com>
2020-09-02 10:04:18 -0700

    multiple additions to python3 script

--- KibanaMain.py
+++ KibanaMain.py
@@ -28,18 +28,21 @@ Multiple_Def_PRE_QA      = ast.literal_eval(MainConfig.get("main","Multiple PREQ
 Multiple_Def_SIG_QA      = ast.literal_eval(MainConfig.get("main","Multiple SIGQA"))
 Multiple_Def_PRE_QA_PROD = ast.literal_eval(MainConfig.get("main","Multiple PREQAPROD"))
 Multiple_Def_SIG_QA_PROD = ast.literal_eval(MainConfig.get("main","Multiple SIGQAPROD"))
+Multiple_MN              = ast.literal_eval(MainConfig.get("main","Multiple MN"))
 
 ConfigSets={'Multiple Rules':Multiple_Rules,
             'Custom Filters':Custom_Filters,
             'Multiple Def PRE QA':Multiple_Def_PRE_QA,
             'Multiple Def SIG QA':Multiple_Def_SIG_QA,
             'Multiple Def PRE QA PROD':Multiple_Def_PRE_QA_PROD,
-            'Multiple Def SIG QA PROD':Multiple_Def_SIG_QA_PROD}
+            'Multiple Def SIG QA PROD':Multiple_Def_SIG_QA_PROD,
+            'Multiple MN':Multiple_MN}
 
 #Query_Test(ConfigSets['Multiple Rules'],'Multiple Rules')
 #Query_Test(ConfigSets['Custom Filters'],'Custom Filters')
-Query_Test(ConfigSets['Multiple Def SIG QA'],'Multiple Def Sig QA')
-Query_Test(ConfigSets['Multiple Def PRE QA'],'Multiple Def Pre QA')
+#Query_Test(ConfigSets['Multiple Def SIG QA'],'Multiple Def Sig QA')
+#Query_Test(ConfigSets['Multiple Def PRE QA'],'Multiple Def Pre QA')
 Query_Test(ConfigSets['Multiple Def SIG QA PROD'],'Multiple Def Sig QA PROD')
 Query_Test(ConfigSets['Multiple Def PRE QA PROD'],'Multiple Def Pre QA PROD')
+#Query_Test(ConfigSets['Multiple MN'],'Multiple MN')
 
