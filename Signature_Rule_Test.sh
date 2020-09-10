3b2a4e6489996ebac7c15444c9f8224e9bb03c21
U-ITSERVICES\jq998g <jq998g@CACDTL05VS0217.ITServices.sbc.com>
2020-09-10 12:57:40 -0700

    signature_rule_sh fixes

--- Signature_Rule_Test.sh
+++ Signature_Rule_Test.sh
@@ -1,11 +1,11 @@
-#OPENGL_Error_var="$(grep 'OPENGL_DL   ERROR' MOST_RECENT_LOG)"
-#echo "$OPENGL_Error_var"
+OPENGL_Error_var="$(grep 'OPENGL_DL   ERROR' MOST_RECENT_LOG)"
+echo "$OPENGL_Error_var"
 #
-#OutOfMemoryError_var="$(grep 'Memory thresholds were broken too much. Rebooting' MOST_RECENT_LOG)"
-#echo "$OutOfMemoryError_var"
+OutOfMemoryError_var="$(grep 'Memory thresholds were broken too much. Rebooting' MOST_RECENT_LOG)"
+echo "$OutOfMemoryError_var"
 #
-#eXtremeDBError_var="$(grep -e 'eXtremeDB' -e 'error' MOST_RECENT_LOG)"
-#echo "$eXtremeDBError_var"
+eXtremeDBError_var="$(grep -e 'eXtremeDB' -e 'error' MOST_RECENT_LOG)"
+echo "$eXtremeDBError_var"
 #
 DISKIOReadError_var="$(grep 'kernel: ata1.00: exception Emask 0x0 SAct 0x0' MOST_RECENT_LOG)"
 echo "$DISKIOReadError_var"
